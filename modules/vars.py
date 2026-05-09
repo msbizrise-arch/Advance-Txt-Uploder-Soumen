711d5b187b5b87fc9e362cad4574a49233f246ed
msbizrise-arch <msbizrise@gmail.com>
2026-05-09 19:09:33 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22480303"))
-API_HASH = environ.get("API_HASH", "99c931b6c1ae6f8c3c3e87da173fa424")
+API_ID = int(environ.get("API_ID", "38498066"))
+API_HASH = environ.get("API_HASH", "c9696114751feacdeb1b4487f5839a1a")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "7518770522"))
+OWNER = int(environ.get("OWNER", "8703802029"))
 CREDIT = environ.get("CREDIT", "𝘽𝙊𝙏𝙎")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '8222519141').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8703802029').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
